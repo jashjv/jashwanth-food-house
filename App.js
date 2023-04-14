9259570bad96876968f745b4335d55ddfd3e7bf2
jashwanth <jashwanth@sameersolutions.com>
2023-04-14 19:09:05 +0530

    added styles

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
+import Slider from "react-slick";
 // import Header from "./components/Header";
 // import Body from "./components/Body";
 
@@ -8,14 +9,14 @@ const Header = () => {
   return (
     <div className="header">
       <div>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQQ9W98Mg2_uVUCWTJY_WkT0adfjOEXCvHRQ&usqp=CAU" className="logo-container"/>
+        <img src="" className="logo-container" />
       </div>
       <div className="nav-items">
         <ul>
-          <li>home</li>
-          <li>about</li>
-          <li>contact us</li>
-          <li>cart</li>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
         </ul>
       </div>
     </div>
@@ -1909,6 +1910,38 @@ const resList = [
 ];
 
 
+const foodsImg = [
+  { obj: 'https://cdn.pixabay.com/photo/2014/06/11/17/00/food-366875__340.jpg' },
+  { obj: 'https://t3.ftcdn.net/jpg/02/97/67/70/360_F_297677001_zX7ZzRq8DObUV5IWTHAIhAae6DuiEQh4.jpg' },
+  { obj: "https://media.istockphoto.com/id/618041312/photo/asian-food-background-with-various-of-cooking-ingredients.jpg?s=170667a&w=0&k=20&c=N12mNgO_oUfWkBKYsGkrx9Zx-R6TM1N9JUq4nvNlQJs=" },
+  { obj: "https://previews.123rf.com/images/kesu87/kesu871907/kesu87190700007/127038773-asian-food-background-with-various-ingredients-on-rustic-wooden-table-top-view.jpg" }
+]
+
+const SliderComponent = () => {
+
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
+  return (
+    <Slider {...settings}>
+      {foodsImg.map((elm) => (
+        <div>
+          <img src={elm.obj} />
+        </div>
+      ))
+      }
+
+    </Slider>
+  )
+
+
+}
+
 const RestroCard = (props) => {
   const { resData } = props;
 
@@ -1924,39 +1957,53 @@ const RestroCard = (props) => {
 
   return (
     <>
-      <div className="res-card">
-       <img src={
-          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
-          cloudinaryImageId
-        } className='res-logo'
-      />
-      <h3>{name}</h3>
-      <h4>{cuisines.join(", ")}</h4>
-      <h4>{avgRating} stars</h4>
-      <h4>₹{costForTwo / 100} FOR TWO</h4>
-      <h4>{deliveryTime} minutes</h4>
-      </div>
+      <body>
+        <div class="maincontainer">
+          <div class="back">
+            <h4>₹{costForTwo / 100} FOR TWO</h4>
+            <span>{cuisines.join(", ")}</span>
+            <h5>{avgRating} stars</h5>
+            <h5>{deliveryTime} minutes Deliver Time</h5>
+          </div>
+          <div class="front">
+            <div class="image">
+              <img src={"https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+                cloudinaryImageId
+              } className='res-logo' />
+              <h3>{name}</h3>
+            </div>
+          </div>
+        </div>
+      </body>
+
     </>
   )
 
 }
 
 
-
-
-
-
-
-
 const Body = () => {
+
+  const [dark, setDark] = useState(true);
+
+  const handleMode = () => {
+    setDark(!dark)
+  }
+
+
   return (
-    <div className="body">
+    <div className={dark === true ? "body" : "notbody"}>
       <div className="search">Search</div>
+      <div onClick={handleMode}>Darkmode</div>
+      {/* <SliderComponent/> */}
       <div className="res-container">
-          {resList.map((restaurant) => (
-          <RestroCard key={restaurant.data.id} resData={restaurant} />
+        {resList.map((restaurant) => (
+          <RestroCard
+            key={restaurant.data.id}
+            resData={restaurant}
+          />
         ))}
-        
+
       </div>
 
     </div>
